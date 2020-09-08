62acf744555981d0b55fab014bfb9c8251d64d84
DYeag <37943859+DYeag@users.noreply.github.com>
2020-09-08 11:58:06 -0700

    use cloudshell-email package
    require and import cloudshell-email

--- package/cloudshell/orch/training/logic/create_user_sandboxes.py
+++ package/cloudshell/orch/training/logic/create_user_sandboxes.py
@@ -7,7 +7,7 @@ from cloudshell.workflow.orchestration.sandbox import Sandbox
 from cloudshell.orch.training.models.config import TrainingWorkflowConfig
 from cloudshell.orch.training.models.position import Position
 from cloudshell.orch.training.models.training_env import TrainingEnvironmentDataModel
-from cloudshell.orch.training.services.email import EmailService
+from cloudshell.orch.email_service.email import EmailService
 from cloudshell.orch.training.services.sandbox_api import SandboxAPIService
 from cloudshell.orch.training.services.sandbox_create import SandboxCreateService
 from cloudshell.orch.training.services.sandbox_output import SandboxOutputService
@@ -54,7 +54,7 @@ class UserSandboxesLogic:
     def _send_emails(self):
         for user in self._env_data.users_list:
             student_link = self._users_data.get_key(user, userDataKeys.STUDENT_LINK)
-            self._email_service.send_email(user, student_link)
+            self._email_service.send_email([user], 'Welcome to Training', student_link)
             self._sandbox_output.notify(f'Sending email to {user} with link={student_link}')
 
     def _wait_for_active_sandboxes_and_add_duplicated_resources(self, sandbox: Sandbox,
